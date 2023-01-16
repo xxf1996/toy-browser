4403de0387946ee8a1a6a5b8d50be36da36fa894
xiexuefeng <1158231926@qq.com>
2023-01-16 18:36:57 +0800

    feat(boa): 调试rust端与js VM端内存双向恢复

--- example/boa-run/object-test.js
+++ example/boa-run/object-test.js
@@ -6,7 +6,10 @@ try {
   console.log(document.appendChild)
   console.log(document.constructor)
   console.log(document.__proto__)
-  const p = document.createElement("p")
+  const link = document.createElement('a')
+  const p = document.createElement('p')
+  console.log(p.node_type)
+  p.appendChild(link)
   document.children[0].appendChild(p)
 
   console.log(JSON.stringify(document, null, 2))
--- example/boa-run/object-test.rs
+++ example/boa-run/object-test.rs
@@ -2,9 +2,12 @@ use std::{path::PathBuf, fs};
 
 use boa_engine::{Context, object::{ObjectInitializer, JsArray, JsFunction}, property::Attribute, prelude::JsObject, JsValue, JsString, JsResult, class::{Class, ClassBuilder}};
 use gc::{ Trace, Finalize, GcCellRef };
-use std::marker::Sized;
+use std::thread;
+use std::time;
 
 /// 模拟DOM节点结构
+///
+/// 相关参考：[Questions about creating DOM types · Issue #2117 · boa-dev/boa](https://github.com/boa-dev/boa/issues/2117)
 #[derive(Debug, Trace, Finalize, Clone)]
 struct DomNode {
   /// 节点类型
@@ -31,18 +34,28 @@ impl DomNode {
   /// 模拟DOM节点原生的appendChild方法
   fn append_child(this: &JsValue, args: &[JsValue], context: &mut Context) -> JsResult<JsValue> {
     // 虽然this和args本身变量都是不可变的，但是可以通过可变的context进行修改
-    let js_node = this.to_object(context).unwrap(); // this对象
-    let js_children_obj = js_node.get("children", context).unwrap();
+    let mut js_node = this.to_object(context).unwrap(); // this对象
+    let js_children_obj = js_node
+      .get("children", context)
+      .unwrap()
+      .to_object(context)
+      .unwrap();
     // 获取到js对象中的子级节点数组对象
-    let js_children = JsArray::from_object(js_children_obj.as_object().unwrap().clone(), context).unwrap();
+    let js_children = JsArray::from_object(js_children_obj, context).unwrap();
     // 参数对象
     let js_new_child = args[0].to_object(context).unwrap();
+    let mut rs_node = js_node
+      .downcast_mut::<DomNode>()
+      .unwrap();
     // 得到参数对应的rust结构
     let rs_new_child = js_new_child.downcast_ref::<DomNode>().unwrap();
-    // 从rust类型转为对应的js对象
-    let child = rs_new_child.to_object(context);
-    js_children.push(child, context).unwrap();
-    js_node.set("children", js_children, false, context).unwrap();
+    rs_node.children.push(rs_new_child.to_owned()); // 同步更新rust结构，否则downcast得到的值就是未更新的
+    drop(rs_new_child); // 释放RefCell
+    drop(rs_node); // 释放可变引用
+    js_children.push(js_new_child, context).unwrap();
+    // js_node.set("children", js_children, false, context).unwrap();
+    let rs_node = js_node.downcast_ref::<DomNode>().unwrap();
+    println!("append_child(downcast_ref struct): {:#?}", rs_node); // NOTICE: 此处downcast得到的结构时更新的，但是不知道为何通过global_object得到的全局对象里面的document值却是未更新的……
     Ok(JsValue::Undefined) // js返回值
   }
 
@@ -77,7 +90,7 @@ impl DomNode {
       node_type,
       children: vec![]
     };
-    Ok(JsValue::Object(node.to_object(context)))
+    Ok(node.to_object2(context))
   }
 
   /// 将当前rust结构转为对应的js对象
@@ -118,14 +131,30 @@ impl DomNode {
     obj
   }
 
-  /// 将js对象恢复成对应的rust结构
-  ///
-  /// FIXME: 这里js对象和rust结构应该无法共用内存？所以只能重建内存？但是为啥downcast_ref返回的又是一个带gc的指针？
-  fn to_origin_data(js_value: JsValue, context: &mut Context) -> DomNode {
-    let js_obj = js_value
-      .as_object()
-      .unwrap();
-    // FIXME: 这里downcast_ref没有恢复vec的结构，为啥？
+  fn to_object2(&self, context: &mut Context) -> JsValue {
+    // 通过构造函数返回一个js对象
+    let constructor = Self::get_constructor(context);
+    let node_type = JsValue::String(JsString::new(self.node_type.clone()));
+    let obj = constructor.construct(&[node_type.clone()], None, context).unwrap();
+    let children = JsArray::new(context);
+    let prototype_obj = self.get_prototype(context);
+    // let prototype = prototype_obj.unwrap();
+    println!("appendChild: {:?}", prototype_obj.has_property("appendChild", context));
+    // 需要手动加上原型对象
+    obj.set_prototype(Some(prototype_obj));
+    // FIXME: 简单的对属性设置属性描述符也无法让属性可以访问，实在是不能理解为啥？
+    obj.set("node_type", node_type, false, context).unwrap();
+    obj.set("children", children, false, context).unwrap();
+    println!("obj is native: {}", obj.is_native_object());
+    let obj_value = JsValue::from(obj);
+    for child in &self.children {
+      let child_value = child.to_object2(context);
+      Self::append_child(&obj_value, &[child_value], context).unwrap(); // NOTICE: 调用原生方法保存rust结构更新
+    }
+    obj_value
+  }
+
+  fn get_rust_data(js_obj: &JsObject, context: &mut Context) -> DomNode {
     let mut rs_node = js_obj
       .downcast_ref::<DomNode>()
       .unwrap()
@@ -136,15 +165,45 @@ impl DomNode {
     let js_children = js_children_obj.as_object().unwrap();
     let children = JsArray::from_object(js_children.clone(), context).unwrap();
     let children_len = children.length(context).unwrap();
+    rs_node.children = vec![];
     if children_len == 0 {
       return rs_node;
     }
     for idx in 0..children_len {
-      let child = children.get(idx, context).unwrap();
-      let rs_child = Self::to_origin_data(child, context);
+      let child = children
+        .get(idx, context)
+        .unwrap()
+        .to_object(context)
+        .unwrap();
+      let rs_child = Self::get_rust_data(&child, context);
       rs_node.children.push(rs_child);
     }
     rs_node
+  }
+
+  /// 将js对象恢复成对应的rust结构
+  fn to_origin_data<'a>(js_obj: &'a JsObject) -> GcCellRef<'a, DomNode> {
+    js_obj.downcast_ref::<DomNode>().unwrap()
+    // FIXME: 这里downcast_ref没有恢复在js环境中修改的值？
+    // let mut rs_node = js_obj
+    //   .downcast_ref::<DomNode>()
+    //   .unwrap()
+    //   .clone();
+    // let js_children_obj = js_obj
+    //   .get("children", context)
+    //   .unwrap();
+    // let js_children = js_children_obj.as_object().unwrap();
+    // let children = JsArray::from_object(js_children.clone(), context).unwrap();
+    // let children_len = children.length(context).unwrap();
+    // if children_len == 0 {
+    //   return rs_node;
+    // }
+    // for idx in 0..children_len {
+    //   let child = children.get(idx, context).unwrap();
+    //   let rs_child = Self::to_origin_data(child, context);
+    //   rs_node.children.push(rs_child);
+    // }
+    // rs_node
   }
 }
 
@@ -178,23 +237,20 @@ fn main() {
   let mut document = DomNode::new("document".to_string());
   body.children.push(div);
   document.children.push(body);
-  let document_object = document.to_object(&mut context);
+  let document_object = document.to_object2(&mut context);
   let shared_str = JsValue::from("test");
-  context.register_global_property("document", document_object, Attribute::READONLY);
-  context.register_global_property("ToyName", shared_str.clone(), Attribute::WRITABLE);
+  context.register_global_property("document", &document_object, Attribute::READONLY);
+  context.register_global_property("ToyName", &shared_str, Attribute::WRITABLE);
   context.eval(&user_script).unwrap();
-  // 全局变量可以通过global_object获取到经过用户脚本修改后的值（本身在rust环境的值并不会自动改变！）
-  println!("{}", context.global_object().clone().get("ToyName", &mut context).unwrap().as_string().unwrap().as_str());
-  let js_document = context
-    .global_object()
-    .clone()
+  // thread::sleep(time::Duration::new(1, 0)); // NOTICE: 原以为global对象的更新有阻塞，但是发现等待一段时间后，结果也没变化
+  let global_boj = context.global_object().clone();
+  let js_document = global_boj
     .get("document", &mut context)
+    .unwrap()
+    .to_object(&mut context)
     .unwrap();
-  // let document = js_document
-  //   .as_object()
-  //   .unwrap()
-  //   .downcast_ref::<DomNode>()
-  //   .unwrap();
-  let document = DomNode::to_origin_data(js_document, &mut context);
-  println!("cur document: {:#?}", document)
+  let document = DomNode::get_rust_data(&js_document, &mut context);
+  println!("cur document: {:#?}", document);
+  // 全局变量可以通过global_object获取到经过用户脚本修改后的值（本身在rust环境的值并不会自动改变！）
+  println!("{}", global_boj.get("ToyName", &mut context).unwrap().as_string().unwrap().as_str());
 }
